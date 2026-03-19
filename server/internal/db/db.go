ee990e035d982f297d3fae3643b25197f475dcb5
李双全 <lishuangquan@yofc.com>
2026-03-19 19:46:54 +0800

    1

--- server/internal/db/db.go
+++ server/internal/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 
 	"clientupdator/server/ent"
@@ -14,8 +15,27 @@ import (
 
 var Client *ent.Client
 
-func GetTxClient() *ent.Client {
-	return Client.Tx()
+func WithTx(ctx context.Context, fn func(tx *ent.Tx) error) error {
+	tx, err := Client.Tx(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if v := recover(); v != nil {
+			tx.Rollback()
+			panic(v)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			err = fmt.Errorf("%w: rolling back transaction: %v", err, rerr)
+		}
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("committing transaction: %w", err)
+	}
+	return nil
 }
 
 func InitDB() {
